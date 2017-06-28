69186b1455f529e9c337be17d5af77f1cac630e7
Houjun <freebirds8281@hotmail.com>
2017-06-28 08:59:38 +0800

    de

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ITask, ITaskConfig, ITaskContext, IContextDefine } from 'development-core';
-export * from './WebTaskOption';
-export declare class WebDefine implements IContextDefine {
-    getContext(config: ITaskConfig): ITaskContext;
-    tasks(ctx: ITaskContext): Promise<ITask[]>;
-}
--- lib/tasks/serve.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="gulp" />
-import { Gulp } from 'gulp';
-import { ITask, ITaskInfo, ITaskContext } from 'development-core';
-export declare class StartServer implements ITask {
-    private info;
-    constructor(info: ITaskInfo);
-    getInfo(): ITaskInfo;
-    setup(ctx: ITaskContext, gulp: Gulp): any;
-}
